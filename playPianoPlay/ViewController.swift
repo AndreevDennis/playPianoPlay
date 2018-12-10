dc52feefc66c6cfa36ef9455eed98ba52a76d048
ArtemVasilenko <artemvasilenko88@gmail.com>
2018-12-10 13:07:10 +0200

    1 commit

--- playPianoPlay/ViewController.swift
+++ playPianoPlay/ViewController.swift
@@ -1,20 +1,42 @@
-//
-//  ViewController.swift
-//  playPianoPlay
-//
-//  Created by Артем on 12/9/18.
-//  Copyright © 2018 Артем. All rights reserved.
-//
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var audioPlayer = AVAudioPlayer()
+    let noteC = Bundle.main.path(forResource: "68441__pinkyfinger__piano-c", ofType: "wav")
+    let noteF = Bundle.main.path(forResource: "f5", ofType: "mp3")
+    
+    @IBOutlet weak var labelNoteName: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    @IBAction func c(_ sender: Any) {
+        labelNoteName.text = nil
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: noteC!))
+        } catch {
+            print(error)
+        }
+        audioPlayer.play()
+        labelNoteName.text?.append("C")
+    }
+    
+    @IBAction func f(_ sender: Any) {
+        labelNoteName.text = nil
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: noteF!))
+        } catch {
+            print(error)
+        }
+        audioPlayer.play()
+        labelNoteName.text?.append("F")
+    }
+    
 }
+
 
